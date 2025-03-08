1f90e6c5d5f870dfe4a3865a1b09a96eda20cb0c
GreedyOrc <ray.banks7@gmail.com>
2025-03-08 14:03:48 +0000

    Login Page now working again - Need to complete register page

--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-login',
@@ -7,4 +9,49 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  constructor() {
+    
+  }
+
+  loading = false;
+  error:string | void = "";
+
+  loginForm = new FormGroup({
+    email: new FormControl("", [Validators.required, Validators.email]),
+    password: new FormControl("", [Validators.required])
+  })
+
+  async onSubit(){
+    this.loading = true;
+    this.error = "";
+    
+  }
+  
+  getEmailError(): string{
+    if(this.loginForm.get('email')?.errors?.['email']){
+      return "Invalid email address"
+    }
+    if(this.loginForm.get('email')?.errors?.['required']){
+      return "Please provide an email address"
+    }
+    return "an unknown error occured"
+  }
+
+  getPasswordError(): string{
+    if(this.loginForm.get('password')?.errors?.['minlength']){
+      return "Invalid password - minimum length is 6 characters"
+    }
+    if(this.loginForm.get('password')?.errors?.['required']){
+      return "Please provide an password"
+    }
+    return "an unknown error occured"
+  }
+
+
+
+  hide = true;
+
+
 }
+
+
