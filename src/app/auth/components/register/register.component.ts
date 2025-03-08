8954da273e9cc5812376bfff27fbfec22889dce0
GreedyOrc <ray.banks7@gmail.com>
2025-03-08 14:32:32 +0000

    Register page now working

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,4 +8,43 @@ import { Component } from '@angular/core';
 })
 export class RegisterComponent {
 
+
+  
+  loading = false;
+  error:string | void = "";
+
+  loginForm = new FormGroup({
+    email: new FormControl("", [Validators.required, Validators.email]),
+    password: new FormControl("", [Validators.required, Validators.minLength(6)])
+  })
+
+  async onSubit(){
+    this.loading = true;
+    this.error = "";
+    
+  }
+  
+  getEmailError(): string{
+    if(this.loginForm.get('email')?.errors?.['email']){
+      return "Invalid email address"
+    }
+    if(this.loginForm.get('email')?.errors?.['required']){
+      return "Please provide an email address"
+    }
+    return "an unknown error occured"
+  }
+
+  getPasswordError(): string{
+    if(this.loginForm.get('password')?.errors?.['minlength']){
+      return "Invalid password - minimum length is 6 characters"
+    }
+    if(this.loginForm.get('password')?.errors?.['required']){
+      return "Please provide an password"
+    }
+    return "an unknown error occured"
+  }
+
+
+
+  hide = true;
 }
